349eff57521fa0ab1306dac155934b3d704ccab5
Thiago Born <thiagofborn@gmail.com>
2020-04-11 22:16:36 +0100

    Adding K8s repo, se linux disable for while, install vim, and docker

---
+++ ks-shell-install/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+set -o errexit
+set -o pipefail
+#
+# description: Sample to install APPs  
+# using KickStart and shell script
+# version: 0.1
+# author: Thiago Fonseca Born da Silva
+# e-mail: thiagofborn@gmail.com
+#-------------------------------------------------
+
+echo "Installing VI improved"
+yum -y install screen vim
+
+echo "Installing Docker"
+curl -fsSL https://get.docker.com | bash
+
+echo "Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+echo "Enable br_netfilter"
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+echo "Adding Kubernetes Official repository $(date) | "
+echo "
+#--------------------------------------------------------------------------------
+# Adding Kubernetes Official repository $(date) |
+#--------------------------------------------------------------------------------
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+
+" >> /etc/yum.repos.d/kubernetes.repo
+
+echo "Installing kubelet kubeadm kubectl"
+yum install -y kubelet kubeadm kubectl
+
+echo "Restarting the systemd daemon and the kubelet service"
+systemctl daemon-reload
+systemctl restart kubelet
